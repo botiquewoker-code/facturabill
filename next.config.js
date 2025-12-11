fee3fef0550332ddf6381524d26a53bfa8289f88
botiquewoker-code <botiquewoker@gmail.com>
2025-12-11 17:24:13 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,14 @@
 module.exports = { swcMinify: true, experimental: { appDir: true } }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Desactiva Turbopack (es lo que rompe Tailwind y react-pdf)
+  experimental: {
+    turbotrace: false,
+    turbo: false,
+  },
+  // Necesario para react-pdf en Vercel
+  serverComponentsExternalPackages: ['@react-pdf/renderer'],
+};
+
+module.exports = nextConfig;
+
