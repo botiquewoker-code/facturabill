b9ceb23afd4e8e9d7d5a9ce78a45b0658d55f56a
botiquewoker-code <botiquewoker@gmail.com>
2025-12-11 17:20:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,1 @@
 module.exports = { swcMinify: true, experimental: { appDir: true } }
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@react-pdf/renderer'],
-  },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
-    return config;
-  },
-  swcMinify: false,
-};
-
-module.exports = nextConfig;
